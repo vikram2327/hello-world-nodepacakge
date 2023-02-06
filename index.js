57d1d29e02c8d50e755a810ae552f0fdfc5e392e
Vikram Pratap Singh <vikram.singh@forethought.in>
2023-02-06 13:21:43 +0530

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.printMsg = function () {
   console.log("This is a message from the demo package");
 };
 
-exports.PrintMsg2 = function () {
-  // export default function PrintMsg2() {
-  return (<div>index</div>);
-};
+// exports.PrintMsg2 = function () {
+//   // export default function PrintMsg2() {
+//   return (<div>index</div>);
+// };

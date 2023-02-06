be13156a8c5aba86571fe78ebef5b241b1954666
Vikram Pratap Singh <vikram.singh@forethought.in>
2023-02-06 13:16:54 +0530

    print msg to added

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 exports.printMsg = function () {
   console.log("This is a message from the demo package");
 };
+
+exports.PrintMsg2 = function () {
+  // export default function PrintMsg2() {
+  return <div>index</div>;
+};
